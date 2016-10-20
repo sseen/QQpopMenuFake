009fb3162414e8b8ac377cd7e501aaa77a53b094
sseen <ssnfee@gmail.com>
2016-10-20 10:02:28 +0800

    start

---
+++ QQpopMenuFake/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QQpopMenuFake
+//
+//  Created by sseen on 2016/10/20.
+//  Copyright © 2016年 sseen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ QQpopMenuFake/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  QQpopMenuFake
+//
+//  Created by sseen on 2016/10/20.
+//  Copyright © 2016年 sseen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
